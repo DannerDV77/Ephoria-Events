b3f6aac65322477176f569c40ac514dfd0cb9a34
DannerDV77 <danieldinosaurio77@gmail.com>
2025-04-30 03:07:03 -0600

    Botones funcionando

--- Scripts/Buttons.js
+++ Scripts/Buttons.js
@@ -1,19 +1,32 @@
 
 document.addEventListener('DOMContentLoaded', function () {
+
     const evento = document.querySelector('.evento');
     evento.addEventListener('click', function () {
         comprarBoletos('Bad Bunny');
     });
+
+    
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    btnContacto = this.getElementById('contacto')
+    
+    btnCrear = this.getElementById('btnCrear')
+
+    btnCrear.addEventListener('click', () => {
+        const url = '/Ephoria-Events/pages/createEvent.html'
+        window.location.href = url
+    })
+
+    btnContacto.addEventListener('click', () => {
+        const url = '/Ephoria-Events/pages/createEvent.html'
+        window.location.href = url
+    })
+    
 });
 
 function comprarBoletos(artista) {
     const url = `/Ephoria-Events/pages/buyTikets.html?artista=${encodeURIComponent(artista)}`;
     window.location.href = url;
-}
-
-
-
-function opencontacto(){
-    const url = '/Ephoria-Events/pages/createEvent.html'
-    window.location.href = url
 }
